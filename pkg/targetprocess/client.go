54982b471da4873760995061ef10fa2840d1e52a
Federico <16429579+rawnly@users.noreply.github.com>
2025-05-16 16:49:54 +0200

    fix: update assignable URL
    fixed a typo in the assignable url generation

--- pkg/targetprocess/client.go
+++ pkg/targetprocess/client.go
@@ -1,15 +1,9 @@
 package targetprocess
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
-	"text/template"
-
-	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
-	"github.com/rawnly/gh-targetprocess/templates"
 )
 
 type transport struct {
@@ -47,71 +41,6 @@ type Client struct {
 	client  *http.Client
 	baseURL string
 	apiKey  string
-}
-
-type Assignable struct {
-	ID          int     `json:"Id"`
-	Name        string  `json:"Name"`
-	Description *string `json:"Description,omitempty"`
-}
-
-func (a *Assignable) URL() string {
-	return fmt.Sprintf("https://www.targetprocess.com/entity/%d", a.ID)
-}
-
-func (a *Assignable) GetPRTitle() string {
-	return fmt.Sprintf("[%d]: %s", a.ID, a.Name)
-}
-
-func (a *Assignable) GetPRBody(baseURL string) string {
-	var buf bytes.Buffer
-
-	tmpl, err := template.New("pr-body").Parse(templates.PRBodyTemplate())
-	if err != nil {
-		return err.Error()
-	}
-
-	description := a.Description
-
-	if description != nil {
-		if strings.Contains(*description, "<!--markdown-->") {
-			*description = strings.Replace(*description, "<!--markdown-->", "", 1)
-		} else {
-			var md string
-			md, err = htmltomarkdown.ConvertString(*description)
-			if err != nil {
-				return err.Error()
-			}
-
-			*description = md
-		}
-	}
-
-	var rows []string
-	if description != nil {
-		rows = strings.Split(*description, "\n\n")
-	}
-
-	payload := struct {
-		ID              int
-		Name            string
-		URL             string
-		Description     *string
-		DescriptionRows []string
-	}{
-		ID:              a.ID,
-		Name:            a.Name,
-		URL:             a.URL(),
-		Description:     description,
-		DescriptionRows: rows,
-	}
-
-	if err = tmpl.Execute(&buf, payload); err != nil {
-		fmt.Println(err.Error())
-		return err.Error()
-	}
-
-	return buf.String()
 }
 
 func New(baseURL string, apiKey string) *Client {
---
+++ pkg/targetprocess/models.go
@@ -0,0 +1,76 @@
+package targetprocess
+
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	"text/template"
+
+	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
+	"github.com/rawnly/gh-targetprocess/templates"
+)
+
+type Assignable struct {
+	ID          int     `json:"Id"`
+	Name        string  `json:"Name"`
+	Description *string `json:"Description,omitempty"`
+}
+
+func (a *Assignable) URL(baseURL string) string {
+	return fmt.Sprintf("%s/entity/%d", a.ID)
+}
+
+func (a *Assignable) GetPRTitle() string {
+	return fmt.Sprintf("[%d]: %s", a.ID, a.Name)
+}
+
+func (a *Assignable) GetPRBody(baseURL string) string {
+	var buf bytes.Buffer
+
+	tmpl, err := template.New("pr-body").Parse(templates.PRBodyTemplate())
+	if err != nil {
+		return err.Error()
+	}
+
+	description := a.Description
+
+	if description != nil {
+		if strings.Contains(*description, "<!--markdown-->") {
+			*description = strings.Replace(*description, "<!--markdown-->", "", 1)
+		} else {
+			var md string
+			md, err = htmltomarkdown.ConvertString(*description)
+			if err != nil {
+				return err.Error()
+			}
+
+			*description = md
+		}
+	}
+
+	var rows []string
+	if description != nil {
+		rows = strings.Split(*description, "\n\n")
+	}
+
+	payload := struct {
+		ID              int
+		Name            string
+		URL             string
+		Description     *string
+		DescriptionRows []string
+	}{
+		ID:              a.ID,
+		Name:            a.Name,
+		URL:             a.URL(baseURL),
+		Description:     description,
+		DescriptionRows: rows,
+	}
+
+	if err = tmpl.Execute(&buf, payload); err != nil {
+		fmt.Println(err.Error())
+		return err.Error()
+	}
+
+	return buf.String()
+}
