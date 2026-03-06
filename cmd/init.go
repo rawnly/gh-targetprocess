0766e8cdbc979760bedd316439200b1f08a647a3
Federico <16429579+rawnly@users.noreply.github.com>
2026-03-06 15:26:36 +0100

    fixes

--- cmd/init.go
+++ cmd/init.go
@@ -5,12 +5,13 @@ package cmd
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"os/exec"
 	"strings"
 	"time"
 
 	"github.com/rawnly/gh-targetprocess/internal"
+	"github.com/rawnly/gh-targetprocess/internal/logging"
 	"github.com/rawnly/gh-targetprocess/internal/utils"
 	"github.com/spf13/cobra"
 )
@@ -21,14 +22,19 @@ func NewInitCmd() *cobra.Command {
 		SilenceErrors: true,
 		SilenceUsage:  true,
 		Example:       "init 210045",
-		Args:          cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx, cancel := context.WithTimeout(cmd.Context(), time.Second*5)
 			defer cancel()
 
+			logf := logging.GetLogger(cmd.OutOrStdout())
+
 			dryRun, err := cmd.Flags().GetBool("dry-run")
 			if err != nil {
 				return err
+			}
+
+			if len(args) == 0 {
+				return errors.New("invalid assignable ID")
 			}
 
 			// 1 fetch the tp-id
@@ -48,7 +54,7 @@ func NewInitCmd() *cobra.Command {
 			}, "/")
 
 			if dryRun {
-				fmt.Fprintf(cmd.OutOrStdout(), "dry-run: %s\n", branchName)
+				logf("dry-run: %s\n", branchName)
 				return nil
 			}
 
